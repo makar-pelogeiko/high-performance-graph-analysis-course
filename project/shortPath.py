afc74bfca8f9cda16ddea8bac779b7f18bcb35f8
makar-pelogeiko <m.pelogeiko@mail.ru>
2022-05-10 19:32:39 +0300

    [Task 2] solution + tests

---
+++ project/shortPath.py
@@ -0,0 +1,40 @@
+import pygraphblas as gb
+from pygraphblas import INT64
+
+__all__ = ["m_get_short_ways", "s_get_short_ways"]
+
+
+def m_get_short_ways(matrix_graph: gb.Matrix, start_verts):
+    no_way = -1
+
+    fronts = gb.Matrix.sparse(
+        matrix_graph.type, nrows=len(start_verts), ncols=matrix_graph.ncols
+    )
+
+    for i, j in enumerate(start_verts):
+        fronts.assign_scalar(0, i, j)
+
+    for _ in range(matrix_graph.nrows):
+        fronts.mxm(
+            matrix_graph,
+            semiring=matrix_graph.type.min_plus,
+            out=fronts,
+            accum=matrix_graph.type.min,
+        )
+
+    result = []
+    for i, j in enumerate(start_verts):
+        temp_step = [no_way] * matrix_graph.nrows
+        fronts[i].to_lists()
+        vert_lst, dist_lst = fronts[i].to_lists()
+        for k, vert in enumerate(vert_lst):
+            temp_step[vert] = dist_lst[k]
+
+        result.append((j, temp_step))
+
+    return result
+
+
+def s_get_short_ways(matrix_graph: gb.Matrix, start_vert: int):
+    result = m_get_short_ways(matrix_graph, [start_vert])
+    return result[0][1]
---
+++ tests/test_short_path.py
@@ -0,0 +1,78 @@
+import pytest
+import pygraphblas as gb
+from project.shortPath import m_get_short_ways, s_get_short_ways
+
+
+@pytest.mark.parametrize(
+    "I, J, V, size, start_verts, expected",
+    [
+        (
+            [0, 0, 0, 1, 1, 1, 2, 2, 2],
+            [0, 1, 2, 0, 1, 2, 0, 1, 2],
+            [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0],
+            3,
+            [0, 1, 2],
+            [(0, [0.0, 1.0, 2.0]), (1, [3.0, 0.0, 5.0]), (2, [6.0, 7.0, 0.0])],
+        ),
+        (
+            [0, 0, 0, 1, 1, 1, 2, 2, 2],
+            [0, 1, 2, 0, 1, 2, 0, 1, 2],
+            [0.3, 1.0, 2.0, 10000.0, 4.0, 5.0, 0.5, 7.0, 8.0],
+            3,
+            [0, 2],
+            [(0, [0.0, 1.0, 2.0]), (2, [0.5, 1.5, 0.0])],
+        ),
+        (
+            [0, 0, 0, 0, 0],
+            [0, 1, 2, 3, 4],
+            [1, 2, 3, 4, 5],
+            10,
+            [0, 1, 2, 3],
+            [
+                (0, [0, 2, 3, 4, 5, -1, -1, -1, -1, -1]),
+                (1, [-1, 0, -1, -1, -1, -1, -1, -1, -1, -1]),
+                (2, [-1, -1, 0, -1, -1, -1, -1, -1, -1, -1]),
+                (3, [-1, -1, -1, 0, -1, -1, -1, -1, -1, -1]),
+            ],
+        ),
+    ],
+)
+def test_multiple_short_ways_search(I, J, V, size, start_verts, expected):
+    matrix_graph = gb.Matrix.from_lists(I, J, V, nrows=size, ncols=size)
+    answer = m_get_short_ways(matrix_graph, start_verts)
+    assert answer == expected
+
+
+@pytest.mark.parametrize(
+    "I, J, V, size, start_vert, expected",
+    [
+        (
+            [0, 0, 0, 1, 1, 1, 2, 2, 2],
+            [0, 1, 2, 0, 1, 2, 0, 1, 2],
+            [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0],
+            3,
+            1,
+            [3.0, 0.0, 5.0],
+        ),
+        (
+            [0, 0, 0, 1, 1, 1, 2, 2, 2],
+            [0, 1, 2, 0, 1, 2, 0, 1, 2],
+            [0.3, 1.0, 2.0, 10000.0, 4.0, 5.0, 0.5, 7.0, 8.0],
+            3,
+            2,
+            [0.5, 1.5, 0.0],
+        ),
+        (
+            [0, 0, 0, 0, 0],
+            [0, 1, 2, 3, 4],
+            [1, 2, 3, 4, 5],
+            10,
+            0,
+            [0, 2, 3, 4, 5, -1, -1, -1, -1, -1],
+        ),
+    ],
+)
+def test_sssp(I, J, V, size, start_vert, expected):
+    matrix_graph = gb.Matrix.from_lists(I, J, V, nrows=size, ncols=size)
+    answer = s_get_short_ways(matrix_graph, start_vert)
+    assert answer == expected
